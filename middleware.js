8e74232218d492f8fd59d06a7fe6605af1001f7e
texasman18 <lsj1092@gmail.com>
2026-07-10 18:29:27 +0900

    테스트7: 401 단독 (헤더 없이)

--- middleware.js
+++ middleware.js
@@ -5,8 +5,5 @@ export const config = {
 export default function middleware() {
   return new Response('Authentication required.', {
     status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Morning Briefing", charset="UTF-8"',
-    },
   });
 }
